7a9805bc57e526489a58c65bd92960b77e311117
Willis Plummer <willisplummer@gmail.com>
2018-05-13 22:21:55 -0400

    delete after merge

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import { forEach, fromEvent, map, filter, pipe } from "callbag-basics";
 
+const clickDeleteBranchButton = () => {
+  const bttn = document.querySelector(".post-merge-message button");
+  bttn && bttn.innerHTML.includes("Delete branch") && bttn.click();
+};
+
 pipe(
   fromEvent(document, "click"),
   filter(ev => ev.target.className.includes("js-merge-commit-button")),
-  forEach(ev => console.log(ev.target.className, ev.target.parentNode.className))
+  forEach(ev => setTimeout(clickDeleteBranchButton, 200))
 );
